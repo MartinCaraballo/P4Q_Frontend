e70961b2b9ef576d59d2b99f1ca452880fd1a03c
nanosanchez22 <nanosanchez22@gmail.com>
2024-12-05 09:57:17 -0300

    modal confirm

---
+++ src/app/Shared/components/modal-confirmado/modal-confirmado.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-confirmado',
+  imports: [],
+  templateUrl: './modal-confirmado.component.html',
+  styleUrl: './modal-confirmado.component.css'
+})
+export class ModalConfirmadoComponent {
+
+}
---
+++ src/app/Shared/components/modal-confirmar/modal-confirmar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-confirmar',
+  imports: [],
+  templateUrl: './modal-confirmar.component.html',
+  styleUrl: './modal-confirmar.component.css'
+  
+})
+export class ModalConfirmarComponent {
+  
+
+}
